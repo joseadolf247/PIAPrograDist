6b4c763670e102f16ce8899b0b6e9423004078cb
Luis Angel Hernandez Martinez <luis.angel.hdz25@gmail.com>
2024-11-11 22:23:35 -0600

    diseno1

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83b90a0eabfc179dfb6cd7f98138a92926b909a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7e35dc2cc151d38e19340867b0bef3bba1690a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
